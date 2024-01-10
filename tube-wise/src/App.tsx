b7ea0b2fba818709f6a0386734b03c42eb0f0419
joana <joanabrito216@gmail.com>
2024-01-10 12:30:07 +0000

    Set up App and fetching the tube data

---
+++ tube-wise/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TubeIndex from './components/TubeIndex';
+// import axios from 'axios'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <TubeIndex />
+      </header>
+    </div>
+  )
+}
+
+export default App;
---
+++ tube-wise/src/components/TubeIndex.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+interface apiProps{
+  name: string
+  statusSeverityDescreption: string
+  modified: string
+}
+
+
+export default function TubeIndex() {
+
+  //* State
+  const [ tube, setTube ] = useState<apiProps[]>()
+
+  //* Initial Render / fetching the data
+  useEffect(() => {
+    async function getTubeData() {
+      try {
+        const { data } = await axios.get('https://api.tfl.gov.uk/Line/Mode/tube/Status')
+        setTube(data)
+        console.log(data)
+      } catch (error) {
+        console.error('Something Went Wrong Please Try Again', error)
+      }
+    }
+    getTubeData()
+  }, [])
+
+  return(
+    <>
+    {tube && tube.length > 0 && (
+      <h3>{tube[0].name}</h3>
+    )}
+    </>
+  )
+}
+
+
