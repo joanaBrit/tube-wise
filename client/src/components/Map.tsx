4d72bd9b83613e9701721ca65b8dfe31792c9198
joana <joanabrito216@gmail.com>
2024-02-26 16:35:07 +0000

    installed and inserted React Zoom Pan Pinch

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,11 +1,43 @@
+import { TransformComponent, TransformWrapper, useControls } from "react-zoom-pan-pinch"
+import { useState } from "react"
+
+// MUI Icons
+import ZoomInIcon from '@mui/icons-material/ZoomIn'
+import ZoomOutIcon from '@mui/icons-material/ZoomOut'
+import ClearIcon from '@mui/icons-material/Clear'
 
 const Map = () => {
 
+  function Controls() {
+    const { zoomIn, zoomOut, resetTransform } = useControls()
+    const [zoomInCount, setzoomInCount] = useState(0)
+    const maxZoom = 5
+
+    return (
+      <>
+        <button onClick={() => zoomIn()} disabled={zoomInCount === maxZoom}><ZoomInIcon/></button>
+        <button onClick={() => zoomOut()}><ZoomOutIcon/></button>
+        <button onClick={() => resetTransform()}><ClearIcon/></button>
+      </>
+    )
+  }
+
   return (
     <>
-    <h1>Map</h1>
-    <h3>Tube Lines</h3>
-    <img src="../../public/assets/tubeMap2024.png" alt="Tube lines map"/>
+      <div className="map">
+        <h1>Map</h1>
+        <h3>Tube Lines</h3>
+      </div>
+      <TransformWrapper>
+        <Controls />
+        <TransformComponent>
+          <img
+            src='/assets/tubeMap2024.png'
+            alt='Tube lines map'
+            width='90%'
+          />
+        </TransformComponent>
+      </TransformWrapper>
     </>
   )
 }
