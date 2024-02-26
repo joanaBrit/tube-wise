bf1667be47b27fc6954a318e7ce331335635c981
joana <joanabrito216@gmail.com>
2024-02-26 11:16:40 +0000

    Inserted text about the App on the Home page

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,11 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <h2>Make wise decisions when planning your journey.</h2>
+    <h3>Optimize your commute with Tube Wise, 
+      ensuring a smoother and more efficient travel experience.</h3>
+    </>
   )
 }
 
