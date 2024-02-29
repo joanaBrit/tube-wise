bbf7fc46d168b0dd11d3ba0ebf4c1d53ba483e5d
joana <joanabrito216@gmail.com>
2024-02-29 17:14:21 +0000

    navigation icons

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,22 +1,35 @@
 // MUI
+import { BottomNavigation, BottomNavigationAction } from '@mui/material'
 import Paper from '@mui/material/Paper'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LanguageIcon from '@mui/icons-material/Language'
 
 
 
 function Footer() {
 
   return (
-
-    <Paper
-      elevation={1}
-      sx={{
-        width: '100%',
-        height: 100,
-        position: 'absolute',
-        bottom: 0,
-      }}
-    />
-
+    <>
+      <Paper
+        elevation={1}
+        sx={{
+          width: '100%',
+          height: 100,
+          position: 'absolute',
+          bottom: 0,
+          backgroundColor: 'rgb(0 0 0 / 100%)'
+        }}
+      >
+        <div className='navigation-button'>
+          <BottomNavigation>
+          <BottomNavigationAction  icon={<LinkedInIcon />} />
+          <BottomNavigationAction  icon={<GitHubIcon />} />
+          <BottomNavigationAction  icon={<LanguageIcon />} />
+          </BottomNavigation>
+        </div>
+      </Paper>
+    </>
   )
 }
 
