98ba2507c781527ec91f4de2f4647942a246ffd3
joana <joanabrito216@gmail.com>
2024-03-01 14:50:36 +0000

    Home Page text styling

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,11 +1,27 @@
+import { Paper } from "@mui/material"
+
+
 
 const Home = () => {
   return (
-    <>
-    <h2>Make wise decisions when planning your journey.</h2>
-    <h3>Optimize your commute with Tube Wise, 
-      ensuring a smoother and more efficient travel experience.</h3>
-    </>
+    <Paper elevation={3}
+      style={{
+        alignItems: 'center',
+        width: '63vw',
+        padding: '2rem',
+        textAlign: 'center',
+        margin: 'auto',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: 'rgb(255 255 255 / 90%)'
+      }}
+    >
+      <div className="app-info" style={{ display: 'flex', flexDirection: 'column' }}>
+        <h2>Make wise decisions when planning your journey.</h2>
+        <h4>Optimize your commute with Tube Wise,
+          ensuring a smoother and more efficient travel experience.</h4>
+      </div>
+    </Paper>
   )
 }
 
