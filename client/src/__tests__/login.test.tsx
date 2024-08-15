a8b08a186d8e89d1f57351898aeb798d21f561b7
Joana Brito <140211258+joanaBrit@users.noreply.github.com>
2024-08-15 16:55:50 +0100

    login testing axios, renders, does a POST

--- client/src/__tests__/login.test.tsx
+++ client/src/__tests__/login.test.tsx
@@ -1,26 +1,40 @@
+import axios from "axios";
 import Login from "../components/Login";
-import { render, screen } from "@testing-library/react";
-// import "@testing-library/jest-dom";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+} from "@testing-library/react";
 
 jest.mock("../utils/router", () => ({
   useNavigate: (v) => console.log("Routing to " + v),
 }));
 
-describe("Login", () => {
-  render(<Login />);
-  it("renders login form", () => {
-    const username = screen.getByRole("textbox", { name: "username" });
-    const email = screen.getByRole("textbox", { name: "email" });
-    const password = screen.getByRole("password", { name: "password" });
-    expect(username).toBeInTheDocument();
-    expect(email).toBeInTheDocument();
-    expect(password).toBeInTheDocument();
+jest.mock("axios");
 
-    it("submits the form with username or email and password", () => {
-      const submitLogin = screen.getByText(
-        "username" || ("email" && "password")
-      );
-      expect(submitLogin).toBeInTheDocument();
-    });
+describe("Login", () => {
+  beforeEach(() => {
+    render(<Login />);
+  });
+
+  it("renders login form", () => {
+    const usernameOrEmail = screen.getByLabelText(/username or email/i);
+    const password = screen.getByLabelText(/password/i);
+    expect(usernameOrEmail).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+  });
+
+  it("does a POST when login button is clicked", async () => {
+    await act(async () =>
+      fireEvent(
+        screen.getByRole("button", { name: "Login" }),
+        new MouseEvent("click", {
+          bubbles: true,
+          cancelable: true,
+        })
+      )
+    );
+    expect(axios.post).toHaveBeenCalledTimes(1);
   });
 });
