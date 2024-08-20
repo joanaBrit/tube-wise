95659ae088b923532d9f6ad88e9cfc8504f9c83c
Joana Brito <140211258+joanaBrit@users.noreply.github.com>
2024-08-20 22:48:29 +0100

    testing journey title, options, axios GET.

--- client/src/__tests__/journey.test.tsx
+++ client/src/__tests__/journey.test.tsx
@@ -1,30 +1,63 @@
 import { act, fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Journey from "../components/Journey";
 import axios from "axios";
-import { MemoryRouter } from "react-router-dom";
 
 jest.mock("axios");
+jest.mock("../utils/auth", () => {
+  return {
+    isLoggedIn: () => {
+      return true;
+    },
+  };
+});
 
 describe("Journey", () => {
   beforeEach(() => {
-    render(
-      <MemoryRouter>
-        <Journey />
-      </MemoryRouter>
-    );
+    render(<Journey />);
   });
 
-  it("renders journey", () => {
-    const fromLocation = screen.getByText("from");
-    const toLocation = screen.getByText("to");
+  it("has a title", () => {
+    const title = screen.getByText("Plan your journey");
+    expect(title).toBeInTheDocument();
+  });
+
+  it("renders journey options", () => {
+    const fromLocation = screen.getByText("From");
+    const toLocation = screen.getByText("To");
     expect(fromLocation).toBeInTheDocument();
     expect(toLocation).toBeInTheDocument();
   });
 
-  it("does a GET when register button is clicked", async () => {
+  it("does not do a GET when plan is clicked if from and to not selected", async () => {
     await act(async () =>
-      fireEvent.click(screen.getByRole("button", { name: /plan/ }))
+      fireEvent.click(screen.getByRole("button", { name: /Plan/ }))
+    );
+    expect(axios.get).toHaveBeenCalledTimes(0);
+  });
+
+  it("does a GET when plan is clicked if from and to selected", async () => {
+    const from = screen.getByRole("combobox", { name: "From" });
+    const to = screen.getByRole("combobox", { name: "To" });
+    expect(from).toBeInTheDocument();
+    expect(to).toBeInTheDocument();
+
+    const selectOption = async (element, option) => {
+      await act(() => userEvent.click(element));
+      const o = await screen.findByText(option);
+      await act(() => userEvent.click(o));
+    };
+
+    await selectOption(from, "Baker Street Underground Station");
+    await selectOption(to, "Charing Cross Underground Station");
+
+    await act(() =>
+      fireEvent.click(screen.getByRole("button", { name: /Plan/ }))
     );
     expect(axios.get).toHaveBeenCalledTimes(1);
+  });
+
+  it("displays all tube lines", () => {
+    //map through the lines
   });
 });
