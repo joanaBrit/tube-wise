42c0bb5bbc20caa792c93c20cd77ed5ce0897a5f
Joana Brito <140211258+joanaBrit@users.noreply.github.com>
2024-08-27 15:10:28 +0100

    Clean code

--- client/src/__tests__/navBar.test.tsx
+++ client/src/__tests__/navBar.test.tsx
@@ -1,52 +1,32 @@
-import { MemoryRouter } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import { pages } from "../components/NavBar";
 import { render, screen } from "@testing-library/react";
 
 describe("NavBar", () => {
   it("renders without crashing", () => {
-    render(
-      <MemoryRouter>
-        <NavBar />
-      </MemoryRouter>
-    );
+    render(<NavBar />);
   });
-});
 
-it("renders all pages", () => {
-  render(
-    <MemoryRouter>
-      <NavBar />
-    </MemoryRouter>
-  );
-  pages.forEach((page) => {
-    expect(screen.getByLabelText(page.label)).toBeInTheDocument();
+  it("renders all pages", () => {
+    pages.forEach((page) => {
+      expect(screen.getByLabelText(page.label)).toBeInTheDocument();
+    });
   });
-});
 
-it("renders all navigation buttons", () => {
-  expect(screen.getByText("Home")).toBeInTheDocument();
-  expect(screen.getByText("Map")).toBeInTheDocument();
-  expect(screen.getByText("Journey")).toBeInTheDocument();
-  expect(screen.getByText("Lines")).toBeInTheDocument();
-});
+  it("renders all navigation buttons", () => {
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Map")).toBeInTheDocument();
+    expect(screen.getByText("Journey")).toBeInTheDocument();
+    expect(screen.getByText("Lines")).toBeInTheDocument();
+  });
 
-it("has a logo", () => {
-  render(
-    <MemoryRouter>
-      <NavBar />
-    </MemoryRouter>
-  );
-  const logo = screen.getByAltText(/Tube London Logo/);
-  expect(logo).toBeInTheDocument();
-});
+  it("has a logo", () => {
+    const logo = screen.getByAltText(/Tube London Logo/);
+    expect(logo).toBeInTheDocument();
+  });
 
-it("has a title", () => {
-  render(
-    <MemoryRouter>
-      <NavBar />
-    </MemoryRouter>
-  );
-  const title = screen.getByText("Tube Wise");
-  expect(title).toBeInTheDocument();
+  it("has a title", () => {
+    const title = screen.getByText("Tube Wise");
+    expect(title).toBeInTheDocument();
+  });
 });
