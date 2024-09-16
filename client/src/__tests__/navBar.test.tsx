b3b9b4443feaccdfb7e1134a1894ad5737d389ca
joana <joanabrito216@gmail.com>
2024-09-16 16:56:00 +0100

    Added a mock to router dom and fixed some tests

--- client/src/__tests__/navBar.test.tsx
+++ client/src/__tests__/navBar.test.tsx
@@ -1,23 +1,29 @@
 import NavBar from "../components/NavBar";
 import { pages } from "../components/NavBar";
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
+
+jest.mock("react-router-dom", () => ({
+  Link: (props: { to; children }) => (
+    <a href={props.to.pathname}>{props.children}</a>
+  ),
+}));
 
 describe("NavBar", () => {
-  it("renders without crashing", () => {
+  beforeEach(() => {
     render(<NavBar />);
   });
+  afterEach(() => cleanup());
 
-  it("renders all pages", () => {
-    pages.forEach((page) => {
-      expect(screen.getByLabelText(page.label)).toBeInTheDocument();
-    });
+  it("renders without crashing", () => {
+    screen.debug();
   });
 
-  it("renders all navigation buttons", () => {
-    expect(screen.getByText("Home")).toBeInTheDocument();
-    expect(screen.getByText("Map")).toBeInTheDocument();
-    expect(screen.getByText("Journey")).toBeInTheDocument();
-    expect(screen.getByText("Lines")).toBeInTheDocument();
+  it("renders all page links", () => {
+    pages.forEach((page) => {
+      expect(
+        screen.getByRole("link", { name: page.label })
+      ).toBeInTheDocument();
+    });
   });
 
   it("has a logo", () => {
@@ -25,8 +31,19 @@ describe("NavBar", () => {
     expect(logo).toBeInTheDocument();
   });
 
+  // it("has a title", () => {
+  //   const title = screen.getByText("Tube Wise");
+  //   expect(title).toBeInTheDocument();
+  // });
+
+  // it("has a visible title", () => {
+  //   // unluck the elements that are not visible
+  //   const title = screen.getByText("Tube Wise", { hidden: false });
+  //   expect(title).toBeVisible();
+  // });
+// Not the best practice
   it("has a title", () => {
-    const title = screen.getByText("Tube Wise");
-    expect(title).toBeInTheDocument();
+    const titles = screen.getAllByText("Tube Wise");
+    expect(titles[0]).toBeInTheDocument();
   });
 });
