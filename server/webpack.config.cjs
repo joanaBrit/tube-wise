02eadbd25f31a9154ab7a71a9d0f48c6b9871e75
joana <joanabrito216@gmail.com>
2024-09-18 12:17:03 +0100

    Update Webpack config for production

--- server/webpack.config.cjs
+++ server/webpack.config.cjs
@@ -1,17 +1,43 @@
+const path = require("path");
+
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+  // mode: "development",
+  // devtool: "inline-source-map",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  devtool: process.env.NODE_ENV === "production" ? false : "inline-source-map",
   entry: "./src/index.ts",
-  target: 'node',
+  target: "node",
   output: {
-    filename: "bundle.cjs"
+    filename: "bundle.cjs",
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
-      { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" }
-    ]
-  }
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
+
+// module.exports = {
+//   entry: './src/index.js', // Entry point of your application
+//   output: {
+
+//     filename: 'bundle.cjs', // Output filename
+//   },
+//   target: 'node', // Ensure it's targeting Node.js
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         use: 'babel-loader', // Assuming you're using Babel
+//       },
+//     ],
+//   },
+// };
